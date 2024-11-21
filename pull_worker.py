67432d9307d7ddee6b1a906ee96dc7dff4764b2c
ericdesoiza <114708774+ericdesoiza@users.noreply.github.com>
2024-11-21 00:10:18 -0600

    replaced threads with multiprocessing pool

--- pull_worker.py
+++ pull_worker.py
@@ -1,6 +1,6 @@
 import sys
-import threading
 import zmq
+import multiprocessing as mp
 
 import dill
 import codecs
@@ -14,26 +14,21 @@ def deserialize(obj: str):
 
 
 class PullWorkers:
-    def __init__(self, n_workers, dispatcher_url):
-        self.n_workers = n_workers
+    def __init__(self, num_worker_processors, dispatcher_url):
+        self.num_worker_processors = num_worker_processors
         self.address = dispatcher_url
         context = zmq.Context()
         self.socket = context.socket(zmq.REQ)
 
     def run(self):
-        self.threads = [threading.Thread(target=self._run_worker, name = idx) 
-                        for idx in range(self.n_workers)]
-        for thread in self._threads:
-            thread.start()
-
-    def _run_worker(self):
-        result = ""
-        while True:
-            task = self._request_task(result)
-            task_object = deserialize(task)
-            params = eval(task_object.params)
-            result_obj = task_object.fn(*params)
-            result = serialize(result_obj)
+        result = "" # Perhaps an indicator of 'Worker is on'
+        with mp.Pool(processes=self.num_worker_processors) as pool:
+            while True:
+                task = self._request_task(result)
+                task_object = deserialize(task)
+                params = eval(task_object.params)
+                result_obj = pool.apply(task_object.fn, (params,))
+                result = serialize(result_obj)
 
     def _request_task(self, task_string):
         self.socket.send_string(task_string)
@@ -46,10 +41,10 @@ if __name__ == "__main__":
         print("Please provide fields: <num_worker_processors> <dispatcher url>")
         sys.exit(1)
 
-    n_workers = int(sys.argv[1])
+    num_worker_processors = int(sys.argv[1])
     dispatcher_url = sys.argv[2]
 
-    workers = PullWorkers(n_workers, dispatcher_url)
+    workers = PullWorkers(num_worker_processors, dispatcher_url)
     workers.run()
 
 
--- push_worker.py
+++ push_worker.py
@@ -1,6 +1,6 @@
 import sys
-import threading
 import zmq
+import multiprocessing as mp
 
 import dill
 import codecs
@@ -14,26 +14,21 @@ def deserialize(obj: str):
 
 
 class PushWorkers:
-    def __init__(self, n_workers, dispatcher_url):
-        self.n_workers = n_workers
+    def __init__(self, num_worker_processors, dispatcher_url):
+        self.num_worker_processors = num_worker_processors
         self.address = dispatcher_url
         context = zmq.Context()
         self.socket = context.socket(zmq.ROUTER)
 
     def run(self):
-        self.threads = [threading.Thread(target=self._run_worker, name = idx) 
-                        for idx in range(self.n_workers)]
-        for thread in self._threads:
-            thread.start()
-
-    def _run_worker(self):
-        while True:
-            id, task = self._receive_task()
-            task_object = deserialize(task)
-            params = eval(task_object.params)
-            result_obj = task_object.fn(*params)
-            result = serialize(result_obj)
-            self._send_result(id, result)
+        with mp.Pool(processes=self.num_worker_processors) as pool:
+            while True:
+                id, task = self._receive_task()
+                task_object = deserialize(task)
+                params = eval(task_object.params)
+                result_obj = pool.apply(task_object.fn, (params,))
+                result = serialize(result_obj)
+                self._send_result(id, result)
 
     def _receive_task(self):
         id, task = self.socket.recv_multipart()
@@ -51,8 +46,8 @@ if __name__ == "__main__":
         print("Please provide fields: <num_worker_processors> <dispatcher url>")
         sys.exit(1)
 
-    n_workers = int(sys.argv[1])
+    num_worker_processors = int(sys.argv[1])
     dispatcher_url = sys.argv[2]
 
-    workers = PushWorkers(n_workers, dispatcher_url)
+    workers = PushWorkers(num_worker_processors, dispatcher_url)
     workers.run()
